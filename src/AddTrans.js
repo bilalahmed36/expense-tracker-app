85fbe5994cf46c9adfb4d762460c10f2dcc2e133
bilalahmed36 <71526359+bilalahmed36@users.noreply.github.com>
2020-11-05 19:23:43 +0500

    udate UI

---
+++ src/AddTrans.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+function AddTrans (){
+
+    let [descr, setDescr] = useState([{Description: '', Amount: ''}]);
+   
+ 
+
+    const addTrans = (e) => {
+        const result = {
+            Description: 'gas Bill',
+            Amount: 200,
+        }
+        setDescr([...descr, result]);
+    };
+
+
+    
+
+    return(
+        <div class='row'>
+            
+            <div class='col-sm'>
+                <div class=' container'>
+                    <p>Transaction History</p>
+                    <hr />
+                    <div class=' container'>
+                        <div>
+                        {descr.map((result, index)=>{
+                            return(
+                                <div key={index}>
+                                    <p>{result.Description} - {result.Amount}</p>
+                                </div>
+                            )
+                        })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='col-sm'>
+                <div class=' container'>
+                    <p>Add Transaction</p>
+                    <hr />
+                    <form >
+                        <label>
+                            Description
+                        </label>
+                        <br />
+                        <input type="text"   />
+                        <br/>
+                        <label>
+                            Transaction Amount
+                        </label>
+                        <br/>
+                        <input type="text"   />
+                        <br />
+                        < button class='btn btn-info mt-3' onClick = {addTrans} >Add</ button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    
+    )
+}
+export default AddTrans;
--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import AddTrans from './AddTrans';
 
 function App() {
+  // let [debit, setDebit] = useState(0);
+  // let [credit, setCredit] = useState(0);
   return (
-      <div className='container-fluid App-header'>
-       <div className='container App text-center'>
-       <nav class="navbar navbar-light bg-light">
-        <span class="navbar-center mb-0 h1 ">Expense Tracker</span>
-      </nav>
-       </div>
-      <div className='contaiiner'>
+      <div className='container-fluid '>
+        <div className='container'>
+           <nav class="navbar bg-info text-white justify-content-center">
+             <h1>Expense Tracker</h1>
+            </nav>
+        </div>
+        <div className='container jumbotron text-secondary mt-5'>
+         <div class='row'>
+            <div class='col-sm justify-content-center'>
+              <div class=' container'>
+                 <p >CURRENT BALANCE</p>
+                 <hr />
+                 <h2 class='text-dark'>$ 0.0</h2>
+              </div>
+            </div>
+            <div class='col-sm justify-content-center'>
+              <div class=' container'>
+                 <div class='row'>
+                 <div class=' col'>
+                   <p>INCOME</p>
+                   <hr />
+                    {/* <h2 class='text-success' >$ {debit}</h2> */}
+                  </div>
+                  <div class=' col'>
+                   <p>EXPENSE</p>
+                   <hr />
+                  {/* <h2 class='text-danger'>$ {credit}</h2> */}
+                  </div>
+                 </div>
 
-      </div>
-      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='container jumbotron  text-secondary mt-5'>
+            <AddTrans />
+        </div>   
+    </div>
+      
   )  
 }
 
